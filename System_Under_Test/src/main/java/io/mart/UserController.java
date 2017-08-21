b16581ca5a19ed8bf13654aabcfadf53ef3acc7f
Alexander Martyushov <alexander.martyushov@bitplaces.com>
2017-08-21 11:02:25 +0200

    Rename System under test

--- System_Under_Test/src/main/java/io/mart/UserController.java
+++ System_Under_Test/src/main/java/io/mart/UserController.java
@@ -1,6 +1,9 @@
 package io.mart;
 
 import io.mart.dto.User;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +19,16 @@ public class UserController {
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(nickname = "createUser", value = "Create a user", notes = "", response = User.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "An array with failed indexes"),
+            @ApiResponse(code = 400, message = "")})
     User createUser(@RequestBody User user){
         return repository.createUsers(user);
     }
 
+    @ApiOperation(nickname = "getUsers", value = "Get users", notes = "", response = User.class, responseContainer = "List")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "An array with failed indexes"),
+            @ApiResponse(code = 400, message = "")})
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     @ResponseBody
     Collection<User> allUsers(){

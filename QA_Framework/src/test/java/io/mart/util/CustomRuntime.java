dce8aea0912e7b360a8ce7eb94147aae17a3ff29
Aleksandr Martiushov <amartyushov@gmail.com>
2017-09-21 15:32:44 +0200

    Hook feature by tags is finished

--- QA_Framework/src/test/java/io/mart/util/CustomRuntime.java
+++ QA_Framework/src/test/java/io/mart/util/CustomRuntime.java
@@ -7,6 +7,7 @@ import cucumber.runtime.io.ResourceLoader;
 import gherkin.formatter.Reporter;
 import gherkin.formatter.model.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +15,21 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
-@Component
 public class CustomRuntime extends Runtime {
 
     @Autowired
     private TagsHolder tagsHolder;
 
+    // constructor will be  called during initialisation of this
+    public static CustomRuntime customRuntime;
+
     public CustomRuntime(ResourceLoader resourceLoader, ClassFinder classFinder, ClassLoader classLoader, RuntimeOptions runtimeOptions) {
         super(resourceLoader, classFinder, classLoader, runtimeOptions);
+        customRuntime = this;
+    }
+
+    public static CustomRuntime getInstance(){
+        return customRuntime;
     }
 
     @Override
